dbabb76c4cfd111d74a12977251692e347f424ff
P-WJ <pwj1103@gmail.com>
2025-09-15 18:29:59 +0900

    first commit

---
+++ Mocar-iOS/Mocar_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mocar_iOSApp.swift
+//  Mocar-iOS
+//
+//  Created by wj on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Mocar_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
