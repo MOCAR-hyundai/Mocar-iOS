5014aae87cbd76fde8570339469015a5191ce05f
유현아 <yha201341@gmail.com>
2025-09-16 11:13:08 +0900

    feat(Auth):회원가입 로그인 마이페이지 UI 구현

---
+++ Mocar-iOS/Features/Auth/Views/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  Mocar-iOS
+//
+//  Created by Admin on 9/16/25.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
